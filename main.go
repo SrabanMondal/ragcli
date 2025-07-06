9345760298c4edc617e1bdd97848c55b820d05c6
Ashank007 <guptaashank38@gmail.com>
2025-07-06 21:36:17 +0530

    Added All Binary Files in build folder

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-//	"github.com/SrabanMondal/ragcli/cmd"
+ "github.com/SrabanMondal/ragcli/cmd"
 )
 func main() {
-	fmt.Print("hello world")
-	//cmd.Execute()
+	cmd.Execute()
 }
